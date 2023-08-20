d8756cdf4aad769994c6affa7e08e8204fffb02a
Tu Nombre <140862540+carosilvariet@users.noreply.github.com>
2023-08-20 18:40:54 -0300

    div en js

---
+++ eventos.js
@@ -0,0 +1,11 @@
+function saludar() {
+    alert('Hola!');
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const divContainer = document.querySelector('.boton-container');
+
+    divContainer.addEventListener('click', function() {
+        alert('Hola! Soy el div');
+    });
+});
